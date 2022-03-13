55f958cbf4fb07e0c1091957e67c91bbec22da8a
Liangcheng Juves <liangchengj@outlook.com>
2022-03-13 23:54:25 +0800

    Revert "Revert "[ Fix ] The default font is not displayed properly""
    This reverts commit ab37a41ceda911de05efdf02853ec40048d46063.

--- tabby-core/src/utils.ts
+++ tabby-core/src/utils.ts
@@ -16,6 +16,7 @@ export function getCSSFontFamily (config: any): string {
     if (config.terminal.fallbackFont) {
         fonts.push(config.terminal.fallbackFont)
     }
+    fonts.push('Tabby Menlo')
     fonts.push('monospace-fallback')
     fonts.push('monospace')
     fonts = fonts.map(x => `"${x}"`)
